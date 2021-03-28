0231919643d55b55ead4508fcc3a53685e7e0805
heather kurth <hmkurth@madisoncollege.edu>
2021-03-28 12:00:13 -0500

    added database sql

---
+++ database.sql
@@ -0,0 +1,37 @@
+create table if not exists artist
+(
+    id varchar(22) not null
+        primary key,
+    artist_name varchar(100) not null,
+    location_id int not null
+);
+
+create table if not exists location
+(
+    id int auto_increment
+        primary key,
+    country varchar(30) not null,
+    region varchar(20) null,
+    city varchar(30) not null
+);
+
+create table if not exists artist_location
+(
+    artist_id varchar(22) not null,
+    location_id int not null,
+    constraint artist_location_artist_id_uindex
+        unique (artist_id),
+    constraint artist_location_location_id_uindex
+        unique (location_id),
+    constraint artist_location_artist_id_fk
+        foreign key (artist_id) references artist (id)
+            on update cascade on delete cascade,
+    constraint artist_location_location_id_fk
+        foreign key (location_id) references location (id)
+            on update cascade on delete cascade
+)
+    comment 'join';
+
+alter table artist_location
+    add primary key (location_id);
+
