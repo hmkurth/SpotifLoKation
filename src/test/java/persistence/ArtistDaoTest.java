fa0e827b0304f44e97c2e21b4f31bff71730a8dd
Simone LaMarche <salamarche@madisoncollege.edu>
2021-03-30 17:01:48 -0500

    adding first test

--- src/test/java/persistence/ArtistDaoTest.java
+++ src/test/java/persistence/ArtistDaoTest.java
@@ -1,18 +1,27 @@
 package persistence;
 
+import entity.Artist;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import test.util.Database;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArtistDaoTest {
-
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    ArtistDao dao;
     @BeforeEach
     void setUp() {
+        dao = new ArtistDao();
+        Database database = Database.getInstance();
+        database.runSQL("cleandb.sql");
     }
 
     @Test
     void getAllArtists() {
+
     }
 
     @Test
@@ -29,5 +38,10 @@ class ArtistDaoTest {
 
     @Test
     void getById() {
+        String id = "5L3STwlqP4cN3E9rMWES48";
+        Artist retrievedArtist = dao.getById(id);
+        assert(retrievedArtist != null);
+        assert(retrievedArtist.getArtist_name().equals("Good Grief"));
+
     }
 }
