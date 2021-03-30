a7e0998d27cf7bb08e78a833f584722eaec66ca4
Simone LaMarche <salamarche@madisoncollege.edu>
2021-03-30 14:21:17 -0500

    test outlines

---
+++ src/test/java/persistence/ArtistDaoTest.java
@@ -0,0 +1,33 @@
+package persistence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArtistDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllArtists() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getById() {
+    }
+}
---
+++ src/test/java/persistence/LocationDaoTest.java
@@ -0,0 +1,33 @@
+package persistence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocationDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllLocations() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getById() {
+    }
+}
