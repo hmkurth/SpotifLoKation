b64d3ecaf2f79c31df60508c3428aadb63422a08
Simone LaMarche <salamarche@madisoncollege.edu>
2021-04-06 18:03:17 -0500

    added logic to check for existing location...not really working

--- src/main/java/com/shk/controller/AddLocation.java
+++ src/main/java/com/shk/controller/AddLocation.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(
         urlPatterns = {"/add-location"}
@@ -30,18 +31,30 @@ public class AddLocation extends HttpServlet {
 
         LocationDao locationDao = new LocationDao();
         ArtistDao artistDao = new ArtistDao();
-
         Artist artist = artistDao.getById(artistId);
+        Location location = null;
 
-        //TODO - Does the entered location already exist?
+        //TODO - This doesn't really work, there's not a good way to check for an existing location
+        List<Location> locations = locationDao.getAllLocations();
+        for (Location l : locations) {
+            String thisCountry = l.getCountry();
+            String thisRegion = l.getRegion();
+            String thisCity = l.getCity();
 
-        Location newLocation = new Location();
-        newLocation.setCountry(country);
-        newLocation.setRegion(region);
-        newLocation.setCity(city);
+            if (thisCountry.equals(country) && thisRegion.equals(region) && thisCity.equals(city)) {
+                location = l;
+            } else {
+                location = new Location();
+                location.setCountry(country);
+                location.setRegion(region);
+                location.setCity(city);
+            }
+        }
 
-        locationDao.insert(newLocation);
-        artist.addLocation(newLocation);
+
+
+        locationDao.insert(location);
+        artist.addLocation(location);
 
         req.setAttribute("artist", artist);
         RequestDispatcher dispatcher = req.getRequestDispatcher("/editArtist.jsp");
