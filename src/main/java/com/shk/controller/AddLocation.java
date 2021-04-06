16a089ebcf3ebd1f786f9efb9d1cce29915ff7a7
Simone LaMarche <salamarche@madisoncollege.edu>
2021-04-06 12:29:21 -0500

    add location logic added

--- src/main/java/com/shk/controller/AddLocation.java
+++ src/main/java/com/shk/controller/AddLocation.java
@@ -1,8 +1,13 @@
 package com.shk.controller;
 
+import com.shk.entity.Artist;
+import com.shk.entity.Location;
+import com.shk.persistence.ArtistDao;
+import com.shk.persistence.LocationDao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,10 +23,29 @@ public class AddLocation extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (req.getParameter("submit").equals("addLocation")){
-            logger.info("add location");
-        }
+        String country = req.getParameter("country");
+        String region = req.getParameter("region");
+        String city = req.getParameter("city");
+        String artistId = req.getParameter("artistId");
 
+        LocationDao locationDao = new LocationDao();
+        ArtistDao artistDao = new ArtistDao();
+
+        Artist artist = artistDao.getById(artistId);
+
+        //TODO - Does the entered location already exist?
+
+        Location newLocation = new Location();
+        newLocation.setCountry(country);
+        newLocation.setRegion(region);
+        newLocation.setCity(city);
+
+        locationDao.insert(newLocation);
+        artist.addLocation(newLocation);
+
+        req.setAttribute("artist", artist);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/editArtist.jsp");
+        dispatcher.forward(req, resp);
 
     }
 }
