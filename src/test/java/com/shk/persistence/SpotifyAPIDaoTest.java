831fff021ac32872d3ad11fe9143f30f8b6d1941
heather kurth <hmkurth@madisoncollege.edu>
2021-04-11 10:57:37 -0500

    restored spotifyapidao and test

---
+++ src/test/java/com/shk/persistence/SpotifyAPIDaoTest.java
@@ -0,0 +1,33 @@
+package com.shk.persistence;
+
+import com.wrapper.spotify.model_objects.specification.Artist;
+import com.wrapper.spotify.model_objects.specification.Image;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+public class SpotifyAPIDaoTest {
+
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    @Test
+    public void testSpotifyAPI() {
+
+        SpotifyAPIDao auth = new SpotifyAPIDao();
+        auth.clientCredentials_Sync();
+
+        Artist returnedArtist = auth.getSpotifyArtist("0LcJLqbBmaGUft1e9Mm8HV");
+
+        //playing around with getting images
+        Image[] images = returnedArtist.getImages();
+
+        for (Image image : images) {
+            logger.info(image.getUrl());
+        }
+
+        assert(returnedArtist.getName().equals("ABBA"));
+
+
+    }
+}
